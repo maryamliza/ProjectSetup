c48490852954e584def6649b53262172f760837f
MaryamLiza <maryam.liza@pucp.pe>
2022-08-19 09:57:53 -0500

    new app

---
+++ app/src/main/java/com/example/projectsetup/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectsetup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
