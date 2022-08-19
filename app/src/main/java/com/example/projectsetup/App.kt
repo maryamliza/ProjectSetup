76be4887342a7f4f4921e28f1eb3a78e742ec0cf
MaryamLiza <maryam.liza@pucp.pe>
2022-08-19 10:12:45 -0500

    setup KOIN

--- app/src/main/java/com/example/projectsetup/App.kt
+++ app/src/main/java/com/example/projectsetup/App.kt
@@ -1,9 +1,18 @@
 package com.example.projectsetup
 
 import android.app.Application
+import com.example.projectsetup.di.repositoryModule
+import com.example.projectsetup.di.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(listOf(repositoryModule, viewModelsModule))
+        }
     }
 }
--- app/src/main/java/com/example/projectsetup/MainActivity.kt
+++ app/src/main/java/com/example/projectsetup/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.projectsetup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectsetup.di.repositoryModule
+import com.example.projectsetup.di.viewModelsModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
